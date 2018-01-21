e61d972e7668668ab4e1f0b6beb351f139a85fcc
Youssef Girgeis <youssefgirgeis@gmail.com>
2018-01-21 13:48:47 -0500

    added more quotes

--- js/quotes.js
+++ js/quotes.js
@@ -3,32 +3,42 @@ var quotes = [
   {
     quote: 'A room without books is like a body without a soul.',
     source: 'Marcus Tullius Cicero',
-    citation: '',
-    year: '',
     tags: ['books', 'soul']
   },
 
   {
     quote: 'You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams.',
     source: 'Dr. Seuss',
-    citation: '',
-    year: '',
     tags: ['dreams', 'reality', 'sleep']
   },
 
   {
     quote: 'Be the change that you wish to see in the world.',
     source: 'Mahatma Gandhi',
-    citation: '',
-    year: '',
     tags: ['change', 'wish', 'inspirational']
   },
 
   {
     quote: 'If you tell the truth, you don\'t have to remember anything.',
     source: 'Mark Twain',
-    citation: '',
-    year: '',
     tags: ['lies', 'memory', 'truth']
+  },
+
+  {
+    quote: 'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.',
+    source: 'Albert Einstein',
+    tags: ['humor', 'science', 'universe', 'infinity', 'stupidity']
+  },
+
+    {
+    quote: 'Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.',
+    source: 'Bil Keane',
+    tags: ['hope', 'inspirational', 'past', 'present']
+  },
+
+    {
+    quote: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.',
+    source: 'Thomas A. Edison',
+    tags: ['failure', 'inspirational']
   }
 ];
