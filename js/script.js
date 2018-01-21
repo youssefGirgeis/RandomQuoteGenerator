71d50935a03f507eb7d421b970861906bdb13897
Youssef Girgeis <youssefgirgeis@gmail.com>
2018-01-21 02:17:22 -0500

    added main structure

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotes = [
+
+  {
+    quote: '',
+    source: '',
+  }
+];
+
+function getRandomQuote(){
+
+  return quote;
+}
+
+
+function printQuote(){
+
+  var quote = getRandomQuote();
+}
