991a468d82f29d5c69afac7a9882ce1ae0e59e1d
Youssef Girgeis <youssefgirgeis@gmail.com>
2018-01-21 11:50:18 -0500

    added new file to keep all quotes in.

---
+++ js/quotes.js
@@ -0,0 +1,34 @@
+var quotes = [
+
+  {
+    quote: 'A room without books is like a body without a soul.',
+    source: 'Marcus Tullius Cicero',
+    citation: '',
+    year: '',
+    tags: ['books', 'soul']
+  },
+
+  {
+    quote: 'You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams.',
+    source: 'Dr. Seuss',
+    citation: '',
+    year: '',
+    tags: ['dreams', 'reality', 'sleep']
+  },
+
+  {
+    quote: 'Be the change that you wish to see in the world.',
+    source: 'Mahatma Gandhi',
+    citation: '',
+    year: '',
+    tags: ['change', 'wish', 'inspirational']
+  },
+
+  {
+    quote: 'If you tell the truth, you don\'t have to remember anything.',
+    source: 'Mark Twain',
+    citation: '',
+    year: '',
+    tags: ['lies', 'memory', 'truth']
+  }
+];
